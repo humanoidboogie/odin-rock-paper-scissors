9b76f53559c0882b9b3a2b4053bd271f211e1633
Colin Wilson <furious.gecko@gmail.com>
2025-04-21 11:40:32 +0100

    Added getHumanChoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,27 @@
-function getComputerChoice()
+function getChoice(choice)
 {
-    // get a random number between 0 and 2
-    let rnd = Math.floor(Math.random() * 3);
-    
     // setup the choices array
     let choices = [ "Rock", "Paper", "Scissors" ];
 
     // return the relevant choice
-    return choices[rnd];
+    return choices[choice];
 }
 
+function getComputerChoice()
+{
+    // get a random number between 0 and 2
+    let randomChoice = Math.floor(Math.random() * 3);
+    
+    return getChoice(randomChoice);
+}
+
+function getHumanChoice()
+{
+    // get the player choice and subtract 1 to get to a choice index
+    let choice = prompt("Choose Rock (1), Paper (2) or Scissors (3): ") - 1;
+
+    return getChoice(choice);
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
